b5623ccbe77d41ca0ad7c409ad8497567ffcf351
Artem Levchenko <levchencode@gmail.com>
2026-02-11 15:44:53 +0200

    pagination utils

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,4 +2,6 @@ const MEGABYTE = 1024 * 1024;
 
 const API_URL = "http://localhost:8080";
 
-export { MEGABYTE, API_URL };
+const DEFAULT_PAGE_SIZE = 4;
+
+export { MEGABYTE, API_URL, DEFAULT_PAGE_SIZE };
